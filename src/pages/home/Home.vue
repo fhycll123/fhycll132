3c5125f667d94f9080ff5287de704e01823fce60
FHY披荆斩棘 <156481154@qq.com>
2018-07-23 15:00:27 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+// template只能暴露一个标签在外面
+<template>
+	<div>
+		<div class="home">hello world</div>
+		<!-- <router-link to="/list" class="home">列表页</router-link> -->
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
