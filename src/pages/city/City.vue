d16769d9be5b62da3b6415663838fbcb8d968b14
FHY披荆斩棘 <156481154@qq.com>
2018-08-22 17:19:12 +0800

    search finish

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
  <div>
   <city-header></city-header>
-  <city-search></city-search>
+  <city-search :city="cities"></city-search>
   <city-list
     :city="cities"
     :hot="hotCities"
