82cdfce2532c2fa5aa6ffe95ff0890f8024f14fa
FHY披荆斩棘 <156481154@qq.com>
2018-08-24 17:29:36 +0800

    use keepalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -17,9 +17,11 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
+    lastCity: '',
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
     HomeIcons: HomeIcons,
@@ -34,9 +36,12 @@ export default {
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -52,7 +57,14 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
